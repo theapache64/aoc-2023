e7e0e6b32190220604c3bf7011a54b2f09d48b1d
theapache64 <theapache64@gmail.com>
2023-12-02 10:26:53 +0530

    ◀️ backup

--- src/_2022/day1/Day01.kt
+++ src/_2022/day1/Day01.kt
@@ -3,14 +3,29 @@ package _2022.day1
 import println
 import readInput
 
+private fun getCalSum(input: List<String>): List<Int> {
+    val calSum = mutableListOf<Int>()
+    var tempCal = 0
+    for (line in input) {
+        if (line.isBlank()) {
+            calSum.add(tempCal)
+            tempCal = 0
+            continue
+        }
+        tempCal += line.toInt()
+    }
+    return calSum
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return -1
+        return getCalSum(input).max()
     }
 
 
     fun part2(input: List<String>): Int {
-        return -1
+        return getCalSum(input).sortedDescending().subList(0, 3)
+            .sum()
     }
 
 
