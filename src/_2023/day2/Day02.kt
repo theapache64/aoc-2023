cbc7c6c1a000740752ea084c8402d03dec8ed1b8
theapache64 <theapache64@gmail.com>
2023-12-02 11:46:08 +0530

    🌟 finish day 2

--- src/_2023/day2/Day02.kt
+++ src/_2023/day2/Day02.kt
@@ -2,6 +2,7 @@ package _2023.day2
 
 import println
 import readInput
+import kotlin.system.exitProcess
 
 private val gameRegex = "Game (?<gameId>\\d+):(?<sets>.+)".toRegex()
 
@@ -23,7 +24,6 @@ fun main() {
             val gameId = gameResult[1].toInt()
             val sets = gameResult[2].split(";")
             gameIds.add(gameId)
-
             for (set in sets) {
                 val colors = set.split(",")
                     .map { it.trim() }
@@ -34,26 +34,40 @@ fun main() {
                     } ?: error("couldn't find color $pickedColor -> '$color'")
 
                     if (pickedCount.toInt() > cube.max) {
-                        println("QuickTag: :part1: wrong game -> '$game' because ${cube.color} can only go ${cube.max}, but found $pickedCount")
+                        // bad game
                         gameIds.remove(gameId)
                         continue@gameLoop
                     }
                 }
             }
-            println("QuickTag: :part1: good game -> $game")
         }
-        println("QuickTag: :part1: $gameIds")
         return gameIds.sum()
     }
 
 
-    fun part2(input: List<String>): Int {
-        return -1
+    fun part2(games: List<String>): Int {
+        val gamePowers = mutableListOf<Int>()
+        gameLoop@ for (game in games) {
+            val gameResult = gameRegex.find(game)?.groupValues ?: error("Invalid data : '$game' ")
+            val sets = gameResult[2].split(";")
+            val gameMap = mutableMapOf<String, MutableList<Int>>()
+            for (set in sets) {
+                val colors = set.split(",").map { it.trim() }
+                for (color in colors) {
+                    val (pickedCount, pickedColor) = color.split(" ")
+                    gameMap.getOrPut(pickedColor, defaultValue = { mutableListOf(pickedCount.toInt()) })
+                        .add(pickedCount.toInt())
+                }
+            }
+            val power = gameMap.values.map { it.max() }.reduce { acc, i -> acc * i }
+            gamePowers.add(power)
+        }
+        return gamePowers.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(path = "src/_2023/day2/Day02_test.txt")
-    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     println("QuickTag: :main: ------------")
     // More tests
@@ -64,33 +78,3 @@ fun main() {
     part2(input).println()
 }
 
-
-private fun parseNumbers(line: String): List<Int> {
-    val numbers = mutableListOf<Int>()
-    var p1 = 0
-    for ((y, i) in (0..line.length).withIndex()) {
-        val word = line.substring(p1, y)
-        val number = getEndsWithInt(word)
-        if (number != null) {
-            p1 = y - 1
-            numbers.add(number)
-        }
-    }
-    return numbers
-}
-
-
-private fun getEndsWithInt(word: String): Int? {
-    return when {
-        word.endsWith("one") || word.endsWith("1") -> 1
-        word.endsWith("two") || word.endsWith("2") -> 2
-        word.endsWith("three") || word.endsWith("3") -> 3
-        word.endsWith("four") || word.endsWith("4") -> 4
-        word.endsWith("five") || word.endsWith("5") -> 5
-        word.endsWith("six") || word.endsWith("6") -> 6
-        word.endsWith("seven") || word.endsWith("7") -> 7
-        word.endsWith("eight") || word.endsWith("8") -> 8
-        word.endsWith("nine") || word.endsWith("9") -> 9
-        else -> null
-    }
-}
