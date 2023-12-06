52aef24cd4908c5b87bacec4df0c91c5c5c305d7
theapache64 <theapache64@gmail.com>
2023-12-06 21:09:59 +0530

    💚 pass day 5 test

--- src/BaseDay.kt
+++ src/BaseDay.kt
@@ -1,4 +1,4 @@
-abstract class BaseDay {
+abstract class BaseDay<T> {
     fun start() {
         val dayNo = this::class.simpleName?.replace("Day", "") ?: error("couldn't find day number from class name")
         println("QuickTag: BaseDay:start: $dayNo")
@@ -7,23 +7,30 @@ abstract class BaseDay {
 
         // Part 1
         val part1TestInput = readInput(path = "src/day$dayNo/Day${dayNo}_part01_test.txt")
-        compare(part1(part1TestInput), part1TestAnswer)
-        part1(input).println()
+        val parsedTestInput = parse(part1TestInput)
+        compare(part1(parsedTestInput), part1TestAnswer)
+        part1(parsedTestInput).println()
 
         // Part 2
         val part2TestInput = readInput(path = "src/day$dayNo/Day${dayNo}_part02_test.txt")
-        compare(part2(part2TestInput), part2TestAnswer)
-        part2(input).println()
+        val parsedInput = parse(part2TestInput)
+        compare(part2(parsedInput), part2TestAnswer)
+        part2(parsedInput).println()
     }
 
     abstract val part1TestAnswer: Int
     abstract val part2TestAnswer: Int
-    abstract fun part1(testInput: List<String>): Int
-    abstract fun part2(testInput: List<String>): Int
+    abstract fun parse(input: List<String>): T
+    abstract fun part1(input: T): Int
+    abstract fun part2(input: T): Int
 
     companion object {
         private fun compare(actual: Int, expected: Int) {
-            println("Wrong answer: Expected '$expected', but actual is '$actual'")
+            if (actual == expected){
+                println("AoC : $expected is the right answer ✅")
+                return
+            }
+            error("Wrong answer: Expected '$expected', but actual is '$actual'")
         }
     }
 }
--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,29 +1,82 @@
 package day05
 
 import BaseDay
-import kotlin.system.exitProcess
 
 fun main() {
     Day05().start()
 }
 
-class Day05 : BaseDay() {
-    override val part1TestAnswer: Int get() = 35
-    override val part2TestAnswer: Int get() = 35
+data class Almanac(
+    val seeds: List<Int>,
+    val maps: List<AgriMap>
+)
 
-    override fun part1(testInput: List<String>): Int {
-        (50..97).forEach {
-            print("$it, ")
-        }
-        println()
-        (52..99).forEach {
-            print("$it, ")
-        }
-        exitProcess(0)
-        return -1
+data class AgriMap(
+    val name: String,
+    val map: Map<Int, Int>
+)
+
+class Day05 : BaseDay<Almanac>() {
+    override val part1TestAnswer: Int get() = 35
+    override val part2TestAnswer: Int get() = 0
+
+    override fun parse(input: List<String>): Almanac {
+        val seeds = input.first().split(": ")[1].split(" ").map { it.toInt() }
+        val maps: List<AgriMap> = input.drop(2)
+            .joinToString(separator = "\n")
+            .split("\n\n")
+            .map {
+                val mapEntries = it.split("\n")
+                val title = mapEntries.first()
+                val mapData = mapEntries.drop(1)
+                val map = mutableMapOf<Int, Int>()
+                for (value in mapData) {
+                    val (destination, source, range) = value.split(" ").map { num -> num.toInt() }
+                    val destRange = (destination..<(destination + range)).toList()
+                    val sourceRange = (source..<(source + range)).toList()
+                    for ((index, src) in sourceRange.withIndex()) {
+                        if(map.containsKey(src)){
+                            error("map already has a value for '$src' -> ${map[src]}")
+                        }
+                        map[src] = destRange[index]
+                    }
+                }
+
+                AgriMap(
+                    name = title,
+                    map = map
+                )
+            }
+        return Almanac(
+            seeds = seeds,
+            maps = maps
+        )
     }
 
-    override fun part2(testInput: List<String>): Int {
+    fun List<String>.splitListByPredicate(predicate: (String) -> Boolean): List<List<String>> {
+        return groupBy { predicate(it) }
+            .values
+            .toList()
+    }
+
+    override fun part1(almanac: Almanac): Int {
+        return almanac
+            .seeds
+            .map { seed ->
+                var x = seed
+                for(map in almanac.maps){
+                    println("QuickTag: Day05:part1: searching in '${map.name}'")
+                    x = map.map[x] ?: x
+                }
+                x
+            }
+            .also {
+                println("QuickTag: locations : $it")
+            }
+            .min() // lowest
+    }
+
+    override fun part2(input: Almanac): Int {
         return -1
     }
 }
