868da64cb4ecf1e9158d657f680a5ad4d624fff1
theapache64 <theapache64@gmail.com>
2023-12-06 19:15:25 +0530

    👕 add tests to base

--- src/BaseDay.kt
+++ src/BaseDay.kt
@@ -1,23 +1,29 @@
 abstract class BaseDay {
     fun start() {
-        val dayNo = this::class.simpleName?.replace("Day","") ?:
-        error("couldn't find day number from class name")
+        val dayNo = this::class.simpleName?.replace("Day", "") ?: error("couldn't find day number from class name")
         println("QuickTag: BaseDay:start: $dayNo")
 
-        // test if implementation meets criteria from the description, like:
-        val testInput = readInput(path = "src/_2023/day$dayNo/Day${dayNo}_test.txt")
-        check(part2(testInput) == 30)
+        val input = readInput(path = "src/day$dayNo/Day${dayNo}.txt")
 
-
-        // More tests
-
-        // Actual data
-        val input = readInput(path = "src/_2023/day$dayNo/Day${dayNo}.txt")
+        // Part 1
+        val part1TestInput = readInput(path = "src/day$dayNo/Day${dayNo}_part01_test.txt")
+        compare(part1(part1TestInput), part1TestAnswer)
         part1(input).println()
+
+        // Part 2
+        val part2TestInput = readInput(path = "src/day$dayNo/Day${dayNo}_part02_test.txt")
+        compare(part2(part2TestInput), part2TestAnswer)
         part2(input).println()
     }
 
-    abstract val testInputAnswer: Int
+    abstract val part1TestAnswer: Int
+    abstract val part2TestAnswer: Int
     abstract fun part1(testInput: List<String>): Int
     abstract fun part2(testInput: List<String>): Int
+
+    companion object {
+        private fun compare(actual: Int, expected: Int) {
+            println("Wrong answer: Expected '$expected', but actual is '$actual'")
+        }
+    }
 }
--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,16 +1,25 @@
 package day05
 
 import BaseDay
+import kotlin.system.exitProcess
 
 fun main() {
     Day05().start()
 }
 
 class Day05 : BaseDay() {
-    override val testInputAnswer: Int
-        get() = 35
+    override val part1TestAnswer: Int get() = 35
+    override val part2TestAnswer: Int get() = 35
 
     override fun part1(testInput: List<String>): Int {
+        (50..97).forEach {
+            print("$it, ")
+        }
+        println()
+        (52..99).forEach {
+            print("$it, ")
+        }
+        exitProcess(0)
         return -1
     }
 
