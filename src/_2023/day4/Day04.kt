dd919dd76c7b50ad7ec4cea6d4c112aadf29d9b7
theapache64 <theapache64@gmail.com>
2023-12-06 16:58:32 +0530

    ◀️ day 4 prepare

--- src/_2023/day4/Day04.kt
+++ src/_2023/day4/Day04.kt
@@ -37,13 +37,34 @@ fun main() {
         return totalPoints
     }
 
-    fun part2(lines: List<String>): Int {
-        return -1
+    fun part2(cards: List<String>): Int {
+        val cardsMap = cards.associateWith { 1 }.toMutableMap()
+        var cardProcessed = 0
+        while (true) {
+            val cardData = cardsMap.entries.find { it.value != 0 } ?: break
+            val card = cardData.key
+            val cardCount = cardData.value
+            cardsMap[card] = cardCount - 1
+            val noOfWins = getNoOfWins(card)
+            cardProcessed++
+
+            if (noOfWins > 0) {
+                // update new card counts
+                val cardIndex = cardsMap.keys.indexOf(card)
+                val subCardKeys = cardsMap.keys.toList().subList(cardIndex + 1, cardIndex + noOfWins + 1)
+                for (subCardKey in subCardKeys) {
+                    cardsMap[subCardKey] = cardsMap[subCardKey]?.plus(1)!!
+                }
+            }
+
+
+        }
+        return cardProcessed
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(path = "src/_2023/day4/Day04_test.txt")
-    check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
 
 
     // More tests
@@ -54,3 +75,27 @@ fun main() {
     part2(input).println()
 }
 
+fun getNoOfWins(card: String): Int {
+    val (winningNumbers, myNumbers) = card
+        .split(":")[1]
+        .split("|")
+        .map {
+            it.trim()
+                .split(" ")
+                .filter { number -> number.isNotBlank() }
+                .map { number ->
+                    number.trim().toInt()
+                }
+        }
+
+    var noOfWins = 0
+    for (winningNumber in winningNumbers) {
+        for (myNumber in myNumbers) {
+            if (winningNumber == myNumber) {
+                noOfWins++
+                print("$winningNumber ")
+            }
+        }
+    }
+    return noOfWins
+}
